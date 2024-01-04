5bc3600029cee4d824fff3f9618ce7a3bc765eea
Enrique <enriquesamudio1999@gmail.com>
2024-01-04 18:11:05 -0300

    Filtering, sorting added to get products

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,12 +3,63 @@ import { productSchema } from '../validations/productValidation.js';
 import { generateSlug, validateObjectId } from '../helpers/index.js';
 
 const getProducts = async (req, res) => {
-  try {
-    const products = await Product.find().sort({ createdAt: 'desc' });
+
+  // Get sort and fields for query
+  const { sort, fields } = req.query;
+
+  // Products Query
+  let productsQuery = Product;
+
+  // Pagination
+  const page = Number(req.query.page) || 1;
+  const LIMIT = 10;
+  const startIndex = (page - 1) * LIMIT;
+
+  // Filtering
+  const filtersObj = { ...req.query };
+  const excludeFields = ['page', 'sort', 'limit', 'fields'];
+  excludeFields.forEach((element) => delete filtersObj[element]);
+  let filters = JSON.stringify(filtersObj);
+  filters = JSON.parse(filters.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`));
+ 
+  // Add filters to query 
+  productsQuery = productsQuery.find(filters);
+
+  // Add sort to query
+  if (sort) {
+    const sortBy = sort.split(',').join(' ');
+    productsQuery = productsQuery.sort(sortBy);
+  } else {
+    productsQuery = productsQuery.sort('-createdAt');
+  }
+
+  // Add fields to query
+  if (fields) { 
+    const fieldsObj = fields.split(',').join(' ');
+    productsQuery = productsQuery.select(fieldsObj);
+  } else {
+    productsQuery = productsQuery.select('-__v');
+  }
+ 
+  try {  
+    const total = await Product.countDocuments(filters);
+ 
+    if (startIndex > total) {
+      return res.status(404).json({
+        success: false,
+        error: 'Invalid page number.'
+      });
+    }
+
+    productsQuery = productsQuery.limit(LIMIT).skip(startIndex);
+    const products = await productsQuery;
 
     res.json({
       success: true, 
-      data: products
+      data: products, 
+      totalProducts: total,
+      currentPage: page,
+      totalPages: Math.ceil(total / LIMIT)
     });
   } catch (error) {
     console.log(error);
